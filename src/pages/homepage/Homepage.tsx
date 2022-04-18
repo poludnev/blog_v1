422a3da4fa19cfcea61e76dcbdc32f93be680662
poludnev <poludnev.roman@gmail.com>
2022-04-18 12:50:38 +0300

    created SignIn page

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogBody from 'src/components/BlogBody/BlogBody';
-import Header from 'src/components/Header/Header';
+import Header1 from 'src/components/Header/Header';
+import Header from 'src/components/Header/HeaderV1';
 import Modal from 'src/components/Modal/Modal';
 import modalModel from 'src/models/modal.model';
+import SignInPage from 'src/pages/SignInPage/SignInPage';
 import { modalTypes } from 'src/types';
+import { useAuth } from 'src/contexts/AuthContext';
 import 'src/pages/homepage/Homepage.styles.scss';
 
 const Homepage = () => {
   const [isShowModal, setShowModal] = useState<boolean>(false);
   const [modalType, setModalType] = useState<modalTypes>('signInModal');
+  // const [isSignedIn, setSignedIn] = useState<boolean>(false);
+  const [isSignedIn, setSignedIn] = useState<boolean>(true);
+  const { currentUser, signOut } = useAuth();
 
   const closeModalHandler = () => {
     setShowModal(false);
@@ -19,6 +25,10 @@ const Homepage = () => {
     setShowModal(true);
   };
 
+  useEffect(() => {
+    setSignedIn(!!currentUser);
+  }, [currentUser]);
+
   return (
     <div className="homepage">
       {isShowModal && (
@@ -27,8 +37,11 @@ const Homepage = () => {
           {...modalModel.getModalProps(modalType)}
         />
       )}
-      <Header showModal={showModalHandler} />
-      <BlogBody />
+      {/* <Header1 showModal={showModalHandler} /> */}
+      <Header />
+      {!isSignedIn && <SignInPage />}
+
+      {isSignedIn && <BlogBody />}
     </div>
   );
 };
